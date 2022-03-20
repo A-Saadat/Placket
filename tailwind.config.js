f55459c013d1644545fa84ccca7ae7be59bdff8c
A-Saadat <ali.saadat.4950@gmail.com>
2022-03-20 18:15:07 +0330

    Add Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["./Src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors:{
+        gray:{
+          250: "#fffcee"
+        }
+      }
     },
   },
   plugins: [],
