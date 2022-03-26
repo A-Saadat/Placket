9b7b5f3f4b1c0c917dd676d70fe7c3d01868c792
A-Saadat <ali.saadat.4950@gmail.com>
2022-03-26 11:03:54 +0430

    Add Product preview page *without Css and Styles*

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,17 @@ module.exports = {
   content: ["./Src/**/*.{html,js}"],
   theme: {
     extend: {
+
       colors:{
         gray:{
           250: "#fffcee"
         }
+      },
+
+      fontFamily:{
+        vazir : "Vazir"
       }
+
     },
   },
   plugins: [],
