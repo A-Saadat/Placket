3560b121883ad2c7963b1061e91a8f6eb679a65c
A-Saadat <ali.saadat.4950@gmail.com>
2022-04-07 14:44:56 +0430

    Add Basic stuff to Login Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
       colors:{
         gray:{
           250: "#fffcee"
+        },
+        slate:{
+          850: "#362C36",
+          550: "#4D4C59",
+          350: "#71777D"
+        },
+        blue:{
+          50: "#A7CBD5"
+        },
+        light:{
+          150: "#F7F6F0"
         }
       },
 
