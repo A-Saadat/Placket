21ce1c873ef2ca7a3084b1b02a37a71f40dc6991
A-Saadat <ali.saadat.4950@gmail.com>
2022-04-20 16:14:44 +0430

    Responsive the Product-card Page

--- Src/Static/Js/main.js
+++ Src/Static/Js/main.js
@@ -1,7 +1,16 @@
 'use strict';
 
 const button = document.querySelector('.burger-btn');
+const dashbord = document.querySelector('.dashbord')
+const body = document.querySelector('body')
+const close_btn = document.querySelector('.close_btn')
 
 button.addEventListener('click', () => {
-	button.classList.toggle('active');
+	dashbord.classList.toggle('toggler');
+	body.classList.toggle('overflow_hidden');
+});
+
+close_btn.addEventListener('click', () => {
+	dashbord.classList.toggle('toggler');
+	body.classList.toggle('overflow_hidden');
 })
